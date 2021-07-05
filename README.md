acf3e0e464a25cfccb482b1875499c83842b5bd2
youngeun <00eun1128@gmail.com>
2021-07-05 15:24:53 +0900

    JS list 출력 및 filter기능 구현

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # shopping-game
 
-쇼핑몰 미니게임 클론코딩 (드림코딩 엘리)
+##간단한 쇼핑몰 미니게임 만들기
+_HTML, CSS, Vanila JS_ 사용한 연습용 웹사이트입니다.
+_21.07.04 ~ 21.07.05_
+
+---
+
+### 중요 기능
+
+- 버튼을 눌렀을 때 해당 버튼 내용으로 filter하기
+- list 스크롤 기능 구현 (overflow-y : scoll)
+
+### 메인화면 RESULT (all clothes)
+
+![메인화면] (.img/readme_1.jpg)
+
+### 메인화면 RESULT (filter by tshirt)
+
+![필터이미지] (.img/readme_2.jpg)
+
+---
+
+이미지 출처 및 프로젝트 정보
+드림코딩 엘리 https://academy.dream-coding.com/
--- src/main.js
+++ src/main.js
@@ -1,105 +1,50 @@
-let list = [
-  {
-    img: "blue_p.png",
-    gender: "man",
-    size: "smail size",
-    color: "blue",
-    type: "pants",
-  },
-  {
-    img: "blue_s.png",
-    gender: "feman",
-    size: "large size",
-    color: "blue",
-    type: "skirt",
-  },
-  {
-    img: "blue_t.png",
-    gender: "man",
-    size: "smail size",
-    color: "blue",
-    type: "tShirt",
-  },
-  {
-    img: "pink_p.png",
-    gender: "man",
-    size: "large size",
-    color: "pink",
-    type: "pants",
-  },
-  {
-    img: "pink_s.png",
-    gender: "man",
-    size: "smail size",
-    color: "pink",
-    type: "skirt",
-  },
-  {
-    img: "pink_t.png",
-    gender: "feman",
-    size: "smail size",
-    color: "pink",
-    type: "tShirt",
-  },
-  {
-    img: "yellow_p.png",
-    gender: "feman",
-    size: "smail size",
-    color: "yellow",
-    type: "pants",
-  },
-  {
-    img: "yellow_s.png",
-    gender: "man",
-    size: "large size",
-    color: "yellow",
-    type: "skirt",
-  },
-  {
-    img: "yellow_t.png",
-    gender: "man",
-    size: "smail size",
-    color: "yellow",
-    type: "tShirt",
-  },
-];
-
-const gnb = document.querySelector(".gnb");
-const clothesList = document.querySelector(".list");
-
-const HIDDEN_CLASS = "hidden";
-
-listPainting();
-
-function listPainting(value) {
-  const ul = document.createElement("ul");
-  list.forEach(function (value) {
-    const li = document.createElement("li");
-    const img = document.createElement("img");
-    const span = document.createElement("span");
-    clothesList.appendChild(ul);
-    ul.appendChild(li);
-    li.appendChild(img);
-    li.appendChild(span);
-    img.src = `../img/${value.img}`;
-    span.innerHTML = `${value.gender} ${value.size}`;
-  });
+//fetch the items from the JSON file
+function loadItems() {
+  return fetch("data/data.json")
+    .then((response) => response.json())
+    .then((json) => json.items);
 }
 
-const tShirt = document.querySelector(".t-shirt");
-const pants = document.querySelector(".pants");
-const skirt = document.querySelector(".skirt");
-const blue = document.querySelector("blue");
-const yellow = document.querySelector(".yellow");
-const pink = document.querySelector(".pink");
-
-function choicePainting(event) {
-  console.dir(event.target);
-  const choiceList = list.filter((choice) => choice.type === "tShirt");
-  console.log(choiceList);
-  // const clothesListUl = document.querySelector(".list ul");
-  // clothesListUl.remove();
-  // listPainting();
+// Update the list with the given items
+function dispalyItems(items) {
+  const container = document.querySelector(".items");
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
-gnb.addEventListener("click", choicePainting);
+// Create HTML list item from the given date item
+function createHTMLString(item) {
+  return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+        <span class="item__description">${item.gender}, ${item.size}</span>
+    </li>
+  `;
+}
+
+// handle button click
+function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+  const filtered = items.filter((item) => item[key] === value);
+  dispalyItems(filtered);
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => dispalyItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
+// main
+loadItems()
+  .then((items) => {
+    dispalyItems(items);
+    setEventListeners(items);
+  })
+  .catch(console.log);
